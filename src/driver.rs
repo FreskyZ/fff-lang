25a2a5b254c5ae7d096ac39b7c89097a38a4a17e
FreskyZ <fresky7@outlook.com>
2016-09-21 12:59:34 +0800

    Add basic physical architeture

---
+++ src/driver.rs
@@ -0,0 +1,5 @@
+
+pub fn compile_file(file_name: String) {
+    
+    println!("Dummy: compiling file: {}", file_name);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,60 @@
+#![allow(dead_code)]
+
+#[macro_use]
+extern crate fsz_common;
+
+mod config;
+mod driver;
+mod lexical;
+mod syntax;
+
+const USAGE_STRING: &'static str = r"
+Usage: 
+
+    smc [inputfile]
+";
+const VERSION_STRING: &'static str = "Fresky's SmallC compiler v0.1.0";
+
+fn print_usage() {
+    println!("{}{}", VERSION_STRING, USAGE_STRING);
+}
+fn print_version() {
+    println!("{}", VERSION_STRING);
+}
+
+// For feel safe
+fn returnable_main() {
+    use std::env::args;
+    use config::Config;
+    use config::ConfigError;
+    use config::CompileFileConfig;
+
+    match Config::from_args(args()) {
+        Ok(Config::Help) => print_usage(),
+        Ok(Config::Version) => print_version(),
+        Err(e @ ConfigError::UnexpectedArgument(..)) => {
+            perrorln!("Error: {}", e);
+        },
+        Ok(Config::CompileFile(CompileFileConfig { file_name })) => {
+            driver::compile_file(file_name);
+        }
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+
+    returnable_main();
+}
+
+#[cfg(test)]
+mod tests {
+    
+    #[test]
+    #[ignore]
+    fn sometest() {
+        use fsz_common::dummyx2;
+
+        assert_eq!(4, dummyx2(2));
+        perrorln!("Something by perrorln");
+    }
 }
