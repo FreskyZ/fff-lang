dbfca38d8813ea0e59b5953a832615595bdd2608
FreskyZ <FreskyZ@outlook.com>
2022-03-22 20:30:22 +0800

    [syntax] fix original intergated tests

--- src/syntax/module.rs
+++ src/syntax/module.rs
@@ -60,53 +60,53 @@ fn module_parse() {
     }
 }
 
-#[cfg(test)] #[test]
+#[cfg(test)]
+#[test]
 fn module_integration() {
-    // use std::fs::File;
-    // use std::io::Read;
+    use std::fmt::Write;
+    use std::fs::read_to_string;
+    use crate::source::{SourceContext};
+    use crate::diagnostics::Diagnostics;
+    use crate::lexical::Parser as LexicalParser;
 
-    // let mut index_file = File::open("tests/syntax/inter/index.txt").expect("cannot open index.txt");
-    // let mut test_cases = String::new();
-    // let _length = index_file.read_to_string(&mut test_cases).expect("cannot read index.txt");
-    // for line in test_cases.lines() {
-    //     let src_path = "tests/syntax/inter/".to_owned() + line + "_src.ff";
-    //     let mut src_file = File::open(&src_path).expect(&format!("cannot open src file {}", src_path));
-    //     let mut src = String::new();
-    //     let _length = src_file.read_to_string(&mut src).expect(&format!("cannot read src file {}", src_path));
-    //     let result_path = "tests/syntax/inter/".to_owned() + line + "_result.txt";
-    //     let mut result_file = File::open(&result_path).expect(&format!("cannot open result file {}", result_path));
-    //     let mut expect = String::new();
-    //     let _length = result_file.read_to_string(&mut expect).expect(&format!("cannot read result file {}", result_path));
-    //     let expect = expect.replace("\r\n", "\n");
+    let test_cases = read_to_string("tests/syntax/inter/index.txt").expect("cannot read index.txt");
+    for line in test_cases.lines() {
+        let expect_display = read_to_string(format!("tests/syntax/inter/{line}_result.txt")).expect(&format!("cannot read {line}_result.txt"));
 
-    //     let result = TestInput::new(&src).apply::<Module, _>().expect_no_message();
-    //     let actual = result.get_result().unwrap().format(Formatter::new(Some(result.get_source()), Some(result.get_symbols())));
-    //     if actual != expect {
-    //         println!("case '{}' failed:", line);
-    //         let (mut linenum, mut actual_iter, mut expect_iter) = (0, actual.split_terminator('\n'), expect.split_terminator('\n'));
-    //         loop {
-    //             match (actual_iter.next(), expect_iter.next()) {
-    //                 (Some(actual_line), Some(expect_line)) if actual_line == expect_line => {
-    //                     println!("={}) {}", linenum, actual_line);
-    //                 }
-    //                 (Some(actual_line), Some(expect_line)) if actual_line != expect_line => {
-    //                     println!("x{}) {:?}", linenum, actual_line);
-    //                     println!("x{}) {:?}", linenum, expect_line);
-    //                 }
-    //                 (Some(_actual_line), Some(_expect_line)) => {
-    //                     panic!("what's the case??");
-    //                 }
-    //                 (Some(actual_line), None) => {
-    //                     println!("^{}) {}", linenum, actual_line);
-    //                 }
-    //                 (None, Some(expect_line)) => {
-    //                     println!("v{}) {}", linenum, expect_line);
-    //                 }
-    //                 (None, None) => break,
-    //             }
-    //             linenum += 1;
-    //         }
-    //         panic!("case failed")
-    //     }
-    // }
+        let mut scx: SourceContext = SourceContext::new(); // this is amazingly real file system
+        let mut ecx = Diagnostics::new();
+        let mut context = ParseContext::new(LexicalParser::new(scx.entry(format!("tests/syntax/inter/{line}.f3")), &mut ecx));
+
+        let actual = Module::parse(&mut context);
+        context.finish();
+        if actual.is_err() {
+            panic!("{}.f3 parse fail: {}", line, ecx.display(&scx));
+        }
+        let actual_display = actual.unwrap().display(&scx).to_string();
+
+        if actual_display != expect_display {
+            let mut buf = format!("{}.f3 display not same\n", line);
+            let (actual_lines, expect_lines) = (actual_display.lines().collect::<Vec<_>>(), expect_display.lines().collect::<Vec<_>>());
+            let common_line_count = std::cmp::min(actual_lines.len(), expect_lines.len());
+            for line in 0..common_line_count {
+                if actual_lines[line] != expect_lines[line] {
+                    write!(buf, "{: >3} |- {}\n", line + 1, actual_lines[line]).unwrap();
+                    write!(buf, "    |+ {}\n", expect_lines[line]).unwrap();
+                } else {
+                    write!(buf, "{: >3} |  {}\n", line + 1, actual_lines[line]).unwrap();
+                }
+            }
+            if actual_lines.len() > common_line_count {
+                for line in common_line_count..actual_lines.len() {
+                    write!(buf, "{: >3} |- {}\n", line + 1, actual_lines[line]).unwrap();
+                }
+            }
+            if expect_lines.len() > common_line_count {
+                for line in common_line_count..expect_lines.len() {
+                    write!(buf, "{: >3} |+ {}\n", line + 1, expect_lines[line]).unwrap();
+                }
+            }
+            panic!("{}", buf)
+        }
+    }
 }
